ffd6cf77c49582ba058fa5583467bced8f75373b
Raghu K <raghuk.vit@gmail.com>
2018-07-05 00:34:53 +0000

    Updating CHEF

--- chef-repo/cookbooks/appservice/recipes/install.rb
+++ chef-repo/cookbooks/appservice/recipes/install.rb
@@ -13,16 +13,47 @@ execute "Extracting tomcat tar file" do
     cwd "/opt"
 end
 
-ruby_block 'Finding files and directories' do
-    block do
-      WEBAPPS=Dir["#{node['appservice']['TAR_PATH']}/webapps/*"]
+
+Dir["#{node['appservice']['TOMCAT_DIR']}/webapps/*"].each do |loc|
+    if File.file?("#{loc}")
+        file "Removing File #{loc}" do
+            path "#{loc}"
+            action :delete
+        end
+    elsif File.directory?("#{loc}") 
+        directory "Removing Directory #{loc}" do
+            path "#{loc}"
+            recursive true
+            action :delete
+        end
     end
-    action :run
+end 
+
+remote_file 'Downloading Student War file' do 
+    path "#{node['appservice']['TOMCAT_DIR']}/webapps/student.war"
+    source "#{node['appservice']['WAR']}"
 end
 
-Dir["#{node['appservice']['TAR_PATH']}/webapps/*"].each  do |app|
-    log 'some messahe' do 
-        message "#{app}"
-        level :fatal
+remote_file 'Downloading JDBC jar file' do 
+    path "#{node['appservice']['TOMCAT_DIR']}/lib/mysql-connector-java-5.1.40.jar"
+    source "#{node['appservice']['JDBC_JAR']}"
+    not_if "ls -l #{node['appservice']['TOMCAT_DIR']}/lib/mysql-connector-java-5.1.40.jar"
+end
+
+template "#{node['appservice']['TOMCAT_DIR']}/conf/context.xml" do
+    source 'context.xml.erb'
+    action :create
+end
+
+if `ps -ef | grep #{node['appservice']['TOMCAT_DIR']} | grep -v grep` != ""
+    execute 'Stop Tomcat' do
+        command "#{node['appservice']['TOMCAT_DIR']}/bin/shutdown.sh"
+    end
+    execute 'Stop Tomcat' do
+        command "#{node['appservice']['TOMCAT_DIR']}/bin/startup.sh"
+    end
+else
+    execute 'Stop Tomcat' do
+        command "#{node['appservice']['TOMCAT_DIR']}/bin/startup.sh"
     end
 end
